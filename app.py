7515d9bbe2a14aa3ccd40634ea0d8d03d472fa48
Salim Oyinlola <salimoyinlola@gmail.com>
2023-07-01 11:02:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask import Flask, redirect, url_for, request, render_template, session
 
 app = Flask(__name__)
 
-
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
